6c1735587395c31563ac9d4f6f380c1baec63fa4
dany <danirazzouqa@gmail.com>
2023-05-24 20:38:08 +0200

    updated index

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php
+session_start();
+require_once 'db_config.php';
+
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,6 +13,20 @@
 </head>
 <body>
     <h1>Welcome to the Photographer Website!</h1>
-    <p><a href="register.php">Register</a> or <a href="login.php">Login</a> to get started.</p>
+    <p><a href="login.php">Login</a> or <a href="register.php">Register</a> to access the dashboard.</p>
+
+    <h2>About</h2>
+    <p>This website is dedicated to photographers who want to showcase their work and connect with their audience.</p>
+
+    <h2>Features</h2>
+    <ul>
+        <li>Register an account to create a profile and manage your photos.</li>
+        <li>Login to access your dashboard and view your uploaded photos.</li>
+        <li>Edit your profile information to personalize your online presence.</li>
+        <li>Add, update, or delete your photos to showcase your portfolio.</li>
+    </ul>
+
+    <h2>Contact</h2>
+    <p>For any inquiries or support, please email us at contact@photographerwebsite.com.</p>
 </body>
 </html>
