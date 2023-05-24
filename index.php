e162bc3c69c69e858c7fc3a1955e6afc2c2d2938
dany <danirazzouqa@gmail.com>
2023-05-24 20:20:00 +0200

    creating files

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Photographer Website</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+    <h1>Welcome to the Photographer Website!</h1>
+    <p><a href="register.php">Register</a> or <a href="login.php">Login</a> to get started.</p>
+</body>
+</html>
