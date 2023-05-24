ea484589f81e90089382962367d0bf6dd82ba5cb
dany <danirazzouqa@gmail.com>
2023-05-24 20:53:08 +0200

    logout/index

--- index.php
+++ index.php
@@ -12,21 +12,35 @@ require_once 'db_config.php';
     <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
-    <h1>Welcome to the Photographer Website!</h1>
-    <p><a href="login.php">Login</a> or <a href="register.php">Register</a> to access the dashboard.</p>
+    <header>
+        <nav>
+            <ul class="header-menu">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="dashboard.php">Dashboard</a></li>
+                <li><a href="profile.php">Profile</a></li>
+                <li><a href="login.php" class="register-login">Register/Login</a></li>
+            </ul>
+        </nav>
+    </header>
 
-    <h2>About</h2>
-    <p>This website is dedicated to photographers who want to showcase their work and connect with their audience.</p>
+    <div class="container">
+        <h1>Welcome to the Photographer Website!</h1>
+        <p><a href="login.php">Login</a> or <a href="register.php">Register</a> to access the dashboard.</p>
 
-    <h2>Features</h2>
-    <ul>
-        <li>Register an account to create a profile and manage your photos.</li>
-        <li>Login to access your dashboard and view your uploaded photos.</li>
-        <li>Edit your profile information to personalize your online presence.</li>
-        <li>Add, update, or delete your photos to showcase your portfolio.</li>
-    </ul>
+        <h2>About</h2>
+        <p>This website is dedicated to photographers who want to showcase their work and connect with their audience.</p>
 
-    <h2>Contact</h2>
-    <p>For any inquiries or support, please email us at contact@photographerwebsite.com.</p>
+        <h2>Features</h2>
+        <ul>
+            <li>Register an account to create a profile and manage your photos.</li>
+            <li>Login to access your dashboard and view your uploaded photos.</li>
+            <li>Edit your profile information to personalize your online presence.</li>
+            <li>Add, update, or delete your photos to showcase your portfolio.</li>
+        </ul>
+
+        <h2>Contact</h2>
+        <p>For any inquiries or support, please email us at contact@photographerwebsite.com.</p>
+    </div>
 </body>
 </html>
